5061e252d55ba6a2ecb680590234a75a98319182
sorabhojha <sorabhojha@gmail.com>
2022-10-21 21:16:37 +0530

    Dummy Commit

--- com/grotechminds/java/FirstJavaApp.java
+++ com/grotechminds/java/FirstJavaApp.java
@@ -4,7 +4,12 @@ public class FirstJavaApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        System.out.print("Hello World");
+        
+        //
+        // Enable collaborataion 
+        // First Step : Adding the file : git add <name of the file> , git add .
+        // Second step: Committing git commit -m "This is a dummy commmit"
+        // git push
 	}
-
 }
