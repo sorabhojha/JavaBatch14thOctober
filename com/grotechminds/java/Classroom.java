d45fe35afad4f413a5a67ed0672316194e95068a
sorabhojha <sorabhojha@gmail.com>
2022-11-01 22:19:19 +0530

    Added static variables

---
+++ com/grotechminds/java/Classroom.java
@@ -0,0 +1,37 @@
+package com.grotechminds.java;
+
+public class Classroom {
+	
+	int strength;
+	int grade;
+	String classTeacher;
+	static int numberOfTotalClassrooms;
+	
+	Classroom(int strength, int grade, String classTeacher){
+		this.strength = strength;
+		this.grade = grade;
+		this.classTeacher = classTeacher;
+		++numberOfTotalClassrooms;
+	}
+
+	public void introduce() {
+		System.out.print("Class Strength: "+strength+" Class Grade: "+grade+" Class Teacher Name: "+classTeacher);
+	}
+	
+	public void createobjects() {
+		Classroom eightGrade = new Classroom(35,8,"Mr. Vijay");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Classroom nineGrade = new Classroom(45,9,"Mr. Amit");
+		//System.out.println(numberOfTotalClassrooms++);
+		
+		Classroom tenthGrade = new Classroom(55,10,"Mr. Sandep");
+		nineGrade.createobjects();
+		//tenthGrade.createobjects();
+		
+		System.out.print("Total number of classes:"+numberOfTotalClassrooms);
+	}
+
+}
